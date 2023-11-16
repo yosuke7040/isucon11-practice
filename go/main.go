b6a9beab6b4de2fee1a1aa39a30958b1006c91e9
yosuke7040 <index_railgu@yahoo.co.jp>
2023-11-17 02:26:15 +0900

    画像データを、*で取得しない様にした

--- go/main.go
+++ go/main.go
@@ -61,14 +61,14 @@ type Config struct {
 }
 
 type Isu struct {
-	ID         int       `db:"id" json:"id"`
-	JIAIsuUUID string    `db:"jia_isu_uuid" json:"jia_isu_uuid"`
-	Name       string    `db:"name" json:"name"`
-	Image      []byte    `db:"image" json:"-"`
-	Character  string    `db:"character" json:"character"`
-	JIAUserID  string    `db:"jia_user_id" json:"-"`
-	CreatedAt  time.Time `db:"created_at" json:"-"`
-	UpdatedAt  time.Time `db:"updated_at" json:"-"`
+	ID         int    `db:"id" json:"id"`
+	JIAIsuUUID string `db:"jia_isu_uuid" json:"jia_isu_uuid"`
+	Name       string `db:"name" json:"name"`
+	// Image      []byte    `db:"image" json:"-"`
+	Character string    `db:"character" json:"character"`
+	JIAUserID string    `db:"jia_user_id" json:"-"`
+	CreatedAt time.Time `db:"created_at" json:"-"`
+	UpdatedAt time.Time `db:"updated_at" json:"-"`
 }
 
 type IsuFromJIA struct {
