d40e0ee9f3a568de96c34dddc194a434c4382eda
yosuke7040 <index_railgu@yahoo.co.jp>
2023-11-21 02:48:08 +0900

    server3 情報

--- s3/deploy.sh
+++ s3/deploy.sh
@@ -4,8 +4,8 @@ set -exu -o pipefail
 
 cd $(dirname $0)
 
-sudo cp -f ./etc/mysql/mariadb.conf.d/50-server.cnf /etc/mysql/mariadb.conf.d/50-server.cnf
-sudo cp -f ./etc/nginx/nginx.conf /etc/nginx/nginx.conf
+#sudo cp -f ./etc/mysql/mariadb.conf.d/50-server.cnf /etc/mysql/mariadb.conf.d/50-server.cnf
+#sudo cp -f ./etc/nginx/nginx.conf /etc/nginx/nginx.conf
 # sudo cp -f ./etc/nginx/sites-available/isucondition.conf /etc/nginx/sites-available/isucondition.conf
 
 #git fetch
@@ -13,5 +13,5 @@ sudo cp -f ./etc/nginx/nginx.conf /etc/nginx/nginx.conf
 cd ../go
 go build -o isucondition main.go
 sudo systemctl restart isucondition.go.service
-sudo systemctl restart nginx
-sudo systemctl restart mariadb
+#sudo systemctl restart nginx
+#sudo systemctl restart mariadb
